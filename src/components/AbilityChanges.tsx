b8a387b52966283fd201854d75c93011d2f8a116
Hristo Meshinski <hristo.meshinski@ffwagency.com>
2023-07-21 12:17:05 +0300

    adjust table styling

--- src/components/AbilityChanges.tsx
+++ src/components/AbilityChanges.tsx
@@ -1,13 +1,14 @@
 import { AbilityChanges as AbilityChangesProps } from "@/app/aramAdjustments/route";
 import React from "react";
 import Image from "next/legacy/image";
+
 type Props = { spells: { [spellName: string]: string } } & AbilityChangesProps;
 
 export const AbilityChanges = (props: Props) => {
   const { abilityName, changes, iconName, spells } = props;
 
   return (
-    <div className="mt-3 flex items-center space-x-3">
+    <div className="flex flex-col items-center space-y-2 md:flex-row md:space-x-3 md:space-y-0">
       <div className="avatar flex-shrink-0">
         <div className="rounded-full ring  ring-offset-2 ring-offset-base-100">
           <Image
@@ -21,9 +22,9 @@ export const AbilityChanges = (props: Props) => {
           />
         </div>
       </div>
-      <div>
+      <div className="">
         <div className="font-bold">{abilityName}</div>
-        <div className="text-sm">{changes}</div>
+        <div className="self-start text-sm">{changes}</div>
       </div>
     </div>
   );
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,6 +4,7 @@ import { AbilityChanges } from "@/components/AbilityChanges";
 import { SearchBar } from "@/components/SearchBar";
 import { ChampionDataScrapped } from "@/app/page";
 import Image from "next/legacy/image";
+
 interface TableProps {
   scrappedData: ChampionDataScrapped;
   winRates: { [key: string]: string };
@@ -25,11 +26,8 @@ export const Table: React.FC<TableProps> = ({
 }) => {
   const [champNames, setChampNames] = useState<string[]>(Object.keys(icons));
   const [searchQuery, setSearchQuery] = useState("");
-  // create a loading state
   const [loading, setLoading] = useState(false);
-  const searchedChampsLen = champNames.length;
-  const scrappedChampsLen = Object.keys(scrappedData).length;
-  const tBodyHeight = searchedChampsLen === 1 ? "" : "h-[440px]";
+
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLoading(true);
     const value = event.target.value;
@@ -39,6 +37,7 @@ export const Table: React.FC<TableProps> = ({
       setLoading(false);
     }
     if (value.length < 2) {
+      setLoading(false);
       return;
     }
 
@@ -49,147 +48,144 @@ export const Table: React.FC<TableProps> = ({
     );
     setLoading(false);
   };
-  const flexStyles = scrappedChampsLen > 0 ? "" : "flex items-center";
+
   return (
-    <div
-      className={`relative flex min-h-[567px]  w-[890px] flex-col rounded-lg bg-gray-950 px-4 pb-4 pt-3 shadow-lg`}
-    >
-      <div className="absolute right-0 top-[-131px]">
-        <Image
-          className=""
-          width={212}
-          height={215}
-          src="/peeking-poro.png"
-          alt={"A poro table background"}
-          title={"A poro peeking behind the table"}
-          placeholder="blur"
-          blurDataURL="/transperant-placeholder.png"
-        />
-      </div>
-      {scrappedChampsLen > 0 ? (
-        <>
-          <div className="w-4xl container max-w-4xl  ">
+    <div className="container max-w-4xl">
+      <div className="relative min-h-[567px] w-full rounded-lg bg-gray-950 px-4 pb-4 pt-3 shadow-lg ">
+        <div className="absolute -right-4 top-[-131px]">
+          <Image
+            className=""
+            width={212}
+            height={215}
+            src="/peeking-poro.png"
+            alt="A poro table background"
+            title="A poro peeking behind the table"
+            placeholder="blur"
+            blurDataURL="/transperant-placeholder.png"
+          />
+        </div>
+        {scrappedData && Object.keys(scrappedData).length > 0 ? (
+          <>
             <div className="mb-3 flex items-center justify-between">
               <SearchBar handleSearch={handleSearch} value={searchQuery} />
-
               <div
-                className="tooltip self-end"
+                className="tooltip ml-2 self-end"
                 data-tip="Leage of Legends patch"
               >
                 <div className="badge">v{version}</div>
               </div>
             </div>
-          </div>
-          <div className="w-4xl container flex max-h-[443px] max-w-4xl flex-grow overflow-auto rounded-lg shadow-md">
-            {searchedChampsLen === 0 && searchQuery !== "" ? (
-              <div
-                className={`flex w-full items-center justify-center text-sm text-gray-500 dark:bg-gray-900 dark:text-gray-400`}
-              >
-                <span>No champions found!</span>
-                <div className="">
-                  <Image
-                    src={"/no-results.png"}
-                    width={64}
-                    height={64}
-                    alt={"A sad poro"}
-                  />
-                </div>
-              </div>
-            ) : !loading ? (
-              <table className="w-full table-fixed text-left text-sm text-gray-500 dark:bg-gray-900 dark:text-gray-400">
-                <thead className="sticky top-0 z-10 bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="w-1/3 px-4 py-3">
-                      Champion
-                    </th>
-                    <th scope="col" className="w-[100px] px-4 py-3">
-                      Win %
-                    </th>
-                    <th scope="col" className="w-[100px] px-1 py-3">
-                      Dmg dealt
-                    </th>
-                    <th scope="col" className="w-[100px] px-1 py-3">
-                      Dmg received
-                    </th>
-                    <th scope="col" className="w-1/3 px-4 py-3">
-                      Other effects
-                    </th>
-                  </tr>
-                </thead>
 
-                <tbody className={` w-full min-w-full overflow-y-scroll`}>
-                  {champNames &&
-                    scrappedData &&
-                    champNames.map((champion, index) => {
-                      const isOdd = index % 2 === 0;
-                      const bgColor = isOdd
-                        ? "dark:bg-gray-900"
-                        : "dark:bg-gray-700";
-                      return (
-                        <tr
-                          key={index}
-                          className={`border-b bg-white ${bgColor}  dark:border-gray-700`}
-                        >
-                          <th
-                            scope="row"
-                            className=" px-4 py-4 font-medium text-gray-900 dark:text-white"
+            <div className="max-h-[443px] w-full overflow-auto rounded-lg shadow-md ">
+              {!loading ? (
+                <table className="table h-full w-full text-left text-sm text-gray-500 dark:bg-gray-900 dark:text-gray-400">
+                  <thead className="sticky top-0 z-10 bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="w-1/3 px-4 py-3">
+                        Champion
+                      </th>
+                      <th scope="col" className="w-[100px] px-4 py-3">
+                        Win %
+                      </th>
+                      <th scope="col" className="w-[100px] px-1 py-3">
+                        Dmg dealt
+                      </th>
+                      <th scope="col" className="w-[100px] px-1 py-3">
+                        Dmg received
+                      </th>
+                      <th scope="col" className="w-1/3 px-4 py-3">
+                        Other effects
+                      </th>
+                    </tr>
+                  </thead>
+
+                  <tbody className=" w-full min-w-full overflow-y-scroll">
+                    {champNames.length === 0 && searchQuery !== "" ? (
+                      <tr>
+                        <td colSpan={5}>
+                          <div className="flex w-full items-center justify-center text-sm text-gray-500 dark:bg-gray-900 dark:text-gray-400">
+                            <span>No champions found!</span>
+                            <div className="">
+                              <Image
+                                src="/no-results.png"
+                                width={64}
+                                height={64}
+                                alt="A sad poro"
+                              />
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    ) : (
+                      champNames.map((champion, index) => {
+                        const isOdd = index % 2 === 0;
+                        const bgColor = isOdd
+                          ? "dark:bg-gray-900"
+                          : "dark:bg-gray-700";
+                        return (
+                          <tr
+                            key={index}
+                            className={`bg-white ${bgColor} dark:border-gray-700`}
                           >
-                            <div className="flex items-center">
-                              <div className="avatar">
-                                <div className="w-24 rounded-full ring  ring-offset-2 ring-offset-base-100">
-                                  <Image
-                                    width={96}
-                                    height={96}
-                                    src={icons[champion].icon || ""}
-                                    alt={champion}
-                                    title={champion}
-                                    placeholder="blur"
-                                    blurDataURL="/champion-placeholder.png"
-                                  />
+                            <th
+                              scope="row"
+                              className="px-4 py-4 font-medium text-gray-900 dark:text-white"
+                            >
+                              <div className="flex items-center">
+                                <div className="avatar hidden md:inline-flex">
+                                  <div className="w-20 rounded-full ring ring-offset-2 ring-offset-base-100">
+                                    <Image
+                                      width={96}
+                                      height={96}
+                                      src={icons[champion]?.icon || ""}
+                                      alt={champion}
+                                      title={champion}
+                                      placeholder="blur"
+                                      blurDataURL="/champion-placeholder.png"
+                                    />
+                                  </div>
+                                </div>
+                                <div className="break-normal px-4">
+                                  <div className="font-bold">{champion}</div>
+                                  <span className="text-sm opacity-50">
+                                    {icons[champion]?.title}
+                                  </span>
                                 </div>
                               </div>
-                              <div className="break-normal	px-4">
-                                <div className="font-bold">{champion}</div>
-                                <span className="text-sm opacity-50">
-                                  {icons[champion]?.title}
-                                </span>
-                              </div>
-                            </div>
-                          </th>
-                          <td className=" px-4 py-4 ">{winRates[champion]}</td>
-                          <td className=" px-4 py-4  text-center">
-                            {champion in scrappedData
-                              ? scrappedData[champion]?.damageDealt
-                              : "0%"}
-                          </td>
-                          <td className=" px-4 py-4  text-center">
-                            {champion in scrappedData
-                              ? scrappedData[champion]?.damageReceived
-                              : "0%"}
-                          </td>
-                          <td className=" px-4 py-4 ">
-                            {champion in scrappedData &&
-                            scrappedData[champion].generalChanges ? (
-                              <span className="flex flex-col space-y-3 pb-2">
-                                {scrappedData[champion]?.generalChanges.map(
-                                  (generalChange, index) => {
-                                    return (
+                            </th>
+                            <td className="px-4 py-4">{winRates[champion]}</td>
+                            <td className="px-4 py-4 text-center">
+                              {champion in scrappedData
+                                ? scrappedData[champion]?.damageDealt || "0%"
+                                : "0%"}
+                            </td>
+                            <td className="px-4 py-4 text-center">
+                              {champion in scrappedData
+                                ? scrappedData[champion]?.damageReceived || "0%"
+                                : "0%"}
+                            </td>
+                            <td className="px-4 py-4">
+                              {champion in scrappedData &&
+                              scrappedData[champion].generalChanges.length >
+                                0 ? (
+                                <span className="flex flex-col space-y-3 pb-2">
+                                  {scrappedData[champion]?.generalChanges.map(
+                                    (generalChange, index) => (
                                       <span
                                         key={generalChange + champion + index}
                                       >
                                         {generalChange}
                                       </span>
-                                    );
-                                  }
-                                )}
-                              </span>
-                            ) : null}
-                            {champion in scrappedData &&
-                            scrappedData[champion].abilityChanges ? (
-                              <span className="flex flex-col space-y-3">
-                                {scrappedData[champion]?.abilityChanges.map(
-                                  (abilityChange, index) => {
-                                    return (
+                                    )
+                                  )}
+                                </span>
+                              ) : null}
+                              {champion in scrappedData &&
+                              Object.keys(scrappedData[champion].abilityChanges)
+                                .length > 0 ? (
+                                <span className="flex flex-col space-y-3">
+                                  {scrappedData[champion]?.abilityChanges.map(
+                                    (abilityChange, index) => (
                                       <AbilityChanges
                                         key={
                                           abilityChange.abilityName +
@@ -199,32 +195,31 @@ export const Table: React.FC<TableProps> = ({
                                         spells={icons[champion]?.spells || {}}
                                         {...abilityChange}
                                       />
-                                    );
-                                  }
-                                )}
-                              </span>
-                            ) : null}
-                            {!(champion in scrappedData) ? (
-                              <>Perfectly balanced, as all things should be</>
-                            ) : null}
-                          </td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
-            ) : (
-              <div className="container flex w-full max-w-4xl items-center justify-center ">
-                <span className="loading loading-spinner loading-lg text-info"></span>
-              </div>
-            )}
-          </div>
-        </>
-      ) : (
-        <div className="container w-full max-w-4xl ">
+                                    )
+                                  )}
+                                </span>
+                              ) : null}
+                              {!(champion in scrappedData) ? (
+                                <>Perfectly balanced, as all things should be</>
+                              ) : null}
+                            </td>
+                          </tr>
+                        );
+                      })
+                    )}
+                  </tbody>
+                </table>
+              ) : (
+                <div className="container flex w-full max-w-4xl items-center justify-center">
+                  <span className="loading loading-spinner loading-lg text-info"></span>
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
           <span className="loading loading-spinner loading-lg text-info"></span>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
