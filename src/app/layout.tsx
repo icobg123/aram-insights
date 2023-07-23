30ed46b3f7ee7ba4e150e1c3800b7345f987970b
Hristo Meshinski <hristo.meshinski@ffwagency.com>
2023-07-23 19:30:59 +0300

    clean up TableBody.tsx add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import GoogleAnalytics from "@/components/google-analytics/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID as string} />
       <body className={inter.className}>{children}</body>
     </html>
   );
