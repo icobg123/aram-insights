faf775235e3f20cd431f52de4aa5c88e0c2176bb
Hristo Meshinski <hristo.meshinski@ffwagency.com>
2024-08-04 12:55:35 +0300

    Add Tabs component for handling tab navigation in TableWrapper

--- src/components/table-wrapper/TableWrapper.tsx
+++ src/components/table-wrapper/TableWrapper.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 import { ItemTableWrapper } from "@/components/table/items/ItemTableWrapper";
 import { ChampionTableWrapper } from "@/components/table/champions/ChampionTableWrapper";
 import { RuneTableWrapper } from "@/components/table/runes/RuneTableWrapper";
@@ -8,6 +7,7 @@ import {
   ItemChangesScrapped,
   RunesChangesScrapped,
 } from "@/types";
+import Tabs from "@/components/table-wrapper/Tabs";
 
 export interface TableWrapperProps {
   championData: ChampionDataApi[];
@@ -16,52 +16,29 @@ export interface TableWrapperProps {
   children: React.ReactNode;
 }
 
+const tabs = ["Champions", "Items", "Runes"];
+
 export const TableWrapper: React.FC<TableWrapperProps> = ({
   championData,
   itemData,
   runeData,
   children,
 }) => {
-  const [activeTab, setActiveTab] = useState<number>(0);
   return (
     <div className="container max-w-5xl p-1">
       <div className="relative flex h-[81svh] min-h-[81svh] w-full flex-col rounded-lg bg-gray-950 px-4 pb-4 pt-3 shadow-lg">
         {children}
-        <div className="tabs flex pb-2">
-          <a
-            className={`tab tab-bordered flex-auto ${
-              activeTab === 0 && "tab-active"
-            }`}
-            onClick={() => setActiveTab(0)}
-          >
-            Champions
-          </a>
-          <a
-            className={`tab tab-bordered flex-auto ${
-              activeTab === 1 && "tab-active"
-            }`}
-            onClick={() => setActiveTab(1)}
-          >
-            Items
-          </a>
-          <a
-            className={`tab tab-bordered flex-auto ${
-              activeTab === 2 && "tab-active"
-            }`}
-            onClick={() => setActiveTab(2)}
-          >
-            Runes
-          </a>
-        </div>
-        <div className="container max-w-5xl">
-          <div className="max-h-[62svh] w-full overflow-auto rounded-lg shadow-md md:max-h-[66svh]">
-            {activeTab === 0 && (
-              <ChampionTableWrapper ChampionDataApi={championData} />
-            )}
-            {activeTab === 1 && <ItemTableWrapper itemData={itemData} />}
-            {activeTab === 2 && <RuneTableWrapper runeData={runeData} />}
-          </div>
-        </div>
+        <Tabs
+          tabLabels={tabs}
+          tabContents={[
+            <ChampionTableWrapper
+              ChampionDataApi={championData}
+              key={tabs[0]}
+            />,
+            <ItemTableWrapper itemData={itemData} key={tabs[1]} />,
+            <RuneTableWrapper runeData={runeData} key={tabs[2]} />,
+          ]}
+        />
       </div>
     </div>
   );
---
+++ src/components/table-wrapper/Tabs.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { ReactNode, useState } from "react";
+
+interface TabsProps {
+  tabLabels: string[];
+  tabContents: ReactNode[];
+}
+
+const Tabs: React.FC<TabsProps> = ({ tabLabels, tabContents }) => {
+  const [activeTab, setActiveTab] = useState<number>(0);
+
+  return (
+    <div className="container max-w-5xl p-1">
+      <div className="relative flex h-[81svh] min-h-[81svh] w-full flex-col rounded-lg bg-gray-950 px-4 pb-4 pt-3 shadow-lg">
+        <div className="tabs flex pb-2">
+          {tabLabels.map((label, index) => (
+            <a
+              key={index}
+              className={`tab-bordered tab flex-auto ${
+                activeTab === index && "tab-active"
+              }`}
+              onClick={() => setActiveTab(index)}
+            >
+              {label}
+            </a>
+          ))}
+        </div>
+        <div className="container max-w-5xl">
+          <div className="max-h-[62svh] w-full overflow-auto rounded-lg shadow-md md:max-h-[66svh]">
+            {tabContents[activeTab]}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
