d0361a363fc001783e4bd7afb1bf7f7a07d7bfdb
Hristo Meshinski <hristo.meshinski@ffwagency.com>
2024-08-05 09:07:45 +0300

    Refactor Tabs component to remove unnecessary container and div elements.

--- src/components/table-wrapper/Tabs.tsx
+++ src/components/table-wrapper/Tabs.tsx
@@ -12,30 +12,28 @@ const Tabs: React.FC<TabsProps> = ({ tabLabels, tabContents }) => {
   const [activeTab, setActiveTab] = useState<number>(0);
 
   return (
-    <div className="container max-w-5xl p-1">
-      <div className="relative flex h-[81svh] min-h-[81svh] w-full flex-col rounded-lg bg-gray-950 px-4 pb-4 pt-3 shadow-lg">
-        <div className="tabs flex pb-2">
-          {tabLabels.map((label, index) => (
-            <a
-              href={`#${tabs[index]}`}
-              key={index}
-              aria-label={tabs[index]}
-              className={`tab-bordered tab flex-auto ${
-                activeTab === index && "tab-active"
-              }`}
-              onClick={() => setActiveTab(index)}
-            >
-              {label}
-            </a>
-          ))}
-        </div>
-        <div className="container max-w-5xl">
-          <div className="max-h-[62svh] w-full overflow-auto rounded-lg shadow-md md:max-h-[66svh]">
-            {tabContents[activeTab]}
-          </div>
+    <>
+      <div className="tabs flex pb-2">
+        {tabLabels.map((label, index) => (
+          <a
+            href={`#${tabs[index]}`}
+            key={index}
+            aria-label={tabs[index]}
+            className={`tab-bordered tab flex-auto ${
+              activeTab === index && "tab-active"
+            }`}
+            onClick={() => setActiveTab(index)}
+          >
+            {label}
+          </a>
+        ))}
+      </div>
+      <div className="container max-w-5xl">
+        <div className="max-h-[62svh] w-full overflow-auto rounded-lg shadow-md md:max-h-[66svh]">
+          {tabContents[activeTab]}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
